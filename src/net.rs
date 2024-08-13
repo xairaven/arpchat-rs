789235025abb0021a15e77792a216b04101f3c82
xairaven <alexkovalevkpi@gmail.com>
2024-08-13 23:16:35 +0300

    Feat: ARP Packet Hardware & Operation Constants

---
+++ src/net.rs
@@ -0,0 +1,5 @@
+const ARP_HTYPE_ETHERNET: u16 = 0x0001; // ARP Hardware type. Ethernet - IEEE 802 Numbers
+const ARP_HADRR_LENGTH: u8 = 6; // ARP Hardware length in bytes. MAC - 6 bytes
+
+const ARP_OPCODE_REQUEST: u8 = 0x0; // ARP Request Operation Code
+const ARP_OPCODE_REPLY: u8 = 0x1; // ARP Reply Operation Code
