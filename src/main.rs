de7ea5a91671b670da63794e258e4116e465ab18
xairaven <alexkovalevkpi@gmail.com>
2024-08-15 20:14:28 +0300

    Feat: Internationalization skel

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 pub mod net;
 pub mod ui;
 
+#[macro_use]
+extern crate rust_i18n;
+
+// Defining folder with locales. Path: crate-root/locales
+rust_i18n::i18n!("locales", fallback = "en");
+
 fn main() {
+    rust_i18n::set_locale("en");
+
     ui::core::start();
 }
