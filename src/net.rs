1389f61f719e269c6e4571301b21a1c096833002
xairaven <alexkovalevkpi@gmail.com>
2024-08-30 04:41:45 +0300

    Feat: Custom ARP Packet Prefix

--- src/net.rs
+++ src/net.rs
@@ -1,8 +1,12 @@
 pub mod interface;
 pub mod ether_type;
 
-const ARP_HTYPE_ETHERNET: u16 = 0x0001; // ARP Hardware type. Ethernet - IEEE 802 Numbers
+const ARP_HTYPE_ETHERNET: &[u8] = &[0x00, 0x01]; // ARP Hardware type. Ethernet - IEEE 802 Numbers
 const ARP_HADRR_LENGTH: u8 = 6; // ARP Hardware length in bytes. MAC - 6 bytes
 
 const ARP_OPCODE_REQUEST: u8 = 0x0; // ARP Request Operation Code
 const ARP_OPCODE_REPLY: u8 = 0x1; // ARP Reply Operation Code
+
+
+// Custom packet prefix
+const PACKET_PREFIX: &[u8] = b"xai";
