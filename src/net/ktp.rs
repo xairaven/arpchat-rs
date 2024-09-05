3a2364909579307d3b93f5baaabf5ab407594411
xairaven <alexkovalevkpi@gmail.com>
2024-09-05 23:46:13 +0300

    Feat: Packet Message and Disconnect types

--- src/net/ktp.rs
+++ src/net/ktp.rs
@@ -23,18 +23,14 @@ const PACKET_DATA_SIZE: usize = (u8::MAX as usize) - PACKET_HEADER_SIZE;
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub enum Packet {
     Message(Id, String),
-    PresenceReq,
-    Presence(Id, bool, String),
     Disconnect(Id),
 }
 
 impl Packet {
     pub fn tag(&self) -> Tag {
         match self {
-            Packet::Message(_, _) => 0,
-            Packet::PresenceReq => 1,
-            Packet::Presence(_, _, _) => 2,
-            Packet::Disconnect(_) => 3,
+            Packet::Disconnect(_) => 0,
+            Packet::Message(_, _) => 1,
         }
     }
 
@@ -43,30 +39,19 @@ impl Packet {
             Packet::Message(id, msg) => {
                 [id as &[u8], &smaz::compress(msg.as_bytes())].concat()
             },
-            Packet::PresenceReq => vec![],
-            Packet::Presence(id, is_join, str) => {
-                [id as &[u8], &[*is_join as u8], str.as_bytes()].concat()
-            },
             Packet::Disconnect(id) => id.to_vec(),
         }
     }
 
     pub fn deserialize(tag: u8, data: &[u8]) -> Option<Self> {
         match tag {
-            0 => {
+            0 => Some(Packet::Disconnect(data.try_into().ok()?)),
+            1 => {
                 let id: Id = data[..size_of::<Id>()].try_into().ok()?;
                 let raw_str = smaz::decompress(&data[size_of::<Id>()..]).ok()?;
                 let str = String::from_utf8(raw_str).ok()?;
                 Some(Packet::Message(id, str))
             },
-            1 => Some(Packet::PresenceReq),
-            2 => {
-                let id: Id = data[..size_of::<Id>()].try_into().ok()?;
-                let is_join = data[size_of::<Id>()] > 0;
-                let str = String::from_utf8(data[size_of::<Id>() + 1..].to_vec()).ok()?;
-                Some(Packet::Presence(id, is_join, str))
-            },
-            3 => Some(Packet::Disconnect(data.try_into().ok()?)),
             _ => None,
         }
     }
